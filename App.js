b07e17a928664b54555fb950381540179161ceb5
Mehdi <112829615+Bixroww@users.noreply.github.com>
2022-10-07 21:52:24 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
-const App =() => {
-  return <h1> Hello React</h1>;
-  };
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './page/About';
+import Home from './page/Home';
+
+const App = () => {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path="/"  element={<Home/>} />
+      <Route path="/about"  element={<About/>} />
+      {/* path="*" fonctionne si jamais l'url ne  correspond à rien de declaré */}
+      <Route path="*"  element={<Home/>} />
+    </Routes>
+   </BrowserRouter>
+  );
+};
 
 export default App;
